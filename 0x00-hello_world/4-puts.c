51599bb7597c9c1e2ac8a96730d2b1892c7b2560
jeremie <jeremiahbht@gmail.com>
2023-02-17 15:43:43 +0200

    helloworld

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,9 @@
+/* this is a c proghram using puts */
 #include <stdio.h>
-/* this is a c program that printis output without use of printf */
-int main(void)
+int main(void/** *entry of the program
+*/)
 {
 puts("\"Programming is like building a multilingual puzzle");
-return (0);
+return (0); /** *the return value
+*/
 }
