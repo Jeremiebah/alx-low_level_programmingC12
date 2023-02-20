f9232d4bcbe18664ebd339044f100238719a7d5c
jeremie <jeremiahbht@gmail.com>
2023-02-20 12:54:23 +0200

    variables

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+/*the program that prints alphabets*/
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(/**
+*the main function
+*/)
+{
+int i;
+for (i = 97; i <= 122; i++)
+{
+putchar(i);
+}
+putchar('\n');
+return (0);
+}
