df06e981d708474a06dbd4320e2f1a1c792af9ba
jeremie <jeremiahbht@gmail.com>
2023-02-20 16:16:00 +0200

    combinations

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,7 +6,7 @@ int main(void/**
 	      *main function entry
 	      */)
 {
-char *message = "and that piece of art is useful\"- Dora Korpar, 2015-10-19\n";
+char *message = "and that piece of art is useful\"- Dora Korpar, 2015-10-19 \n";
 write(2, message, 58);
 return (1);
 }
