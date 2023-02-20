56a1cf947fe9b5383d8835cb2703bda314d4de19
jeremie <jeremiahbht@gmail.com>
2023-02-20 09:54:05 +0200

    size

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,9 @@
+/*header line*/
+#include <unistd.h>
+
+int main(void /*main functions*/)
+{
+const char* message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+    write(STDERR_FILENO, message, sizeof(message)-1);
+    return (1);
+}
