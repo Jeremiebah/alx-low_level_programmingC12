c1d40deef377dc9f927eb4ce518c4dd3cb3a220c
jeremie <jeremiahbht@gmail.com>
2023-02-20 20:03:06 +0200

    combination

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,7 +6,7 @@ int main(void/**
 	      *main function entry
 	      */)
 {
-char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19" ;
+char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
 write(2, message, 59);
 return (1);
 }
