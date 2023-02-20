342172d7b161af33a47a6a21f13df49ad1b4a93b
jeremie <jeremiahbht@gmail.com>
2023-02-20 15:26:32 +0200

    kombo99

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,32 @@
+/*a program that prints all possible combinations of two two-digit numbers*/
+#include <stdio.h>
+
+int main(void/**
+	      *main entry function
+	      */)
+{
+int i, j;
+for (i = 0; i < 100; i++)
+{
+for (j = i; j < 100; j++)
+{
+if (i != j)
+{
+putchar((i / 10) + '0');
+putchar((i % 10) + '0');
+putchar(' ');
+putchar((j / 10) + '0');
+putchar((j % 10) + '0');
+if (i < 99 || j < 99)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+{
+putchar('\n');
+}
+return (0);
+}
