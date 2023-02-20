7fe6e6be2a38d7aa285834fe01df69efd8e4d2bd
jeremie <jeremiahbht@gmail.com>
2023-02-20 16:11:16 +0200

    combinations

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,12 @@
-/*header line*/
+/*heading*/
+#include <stdio.h>
 #include <unistd.h>
-#include <stdlib.h>
 
-int main(void /**
-*main functions
-*/)
+int main(void/**
+	      *main function entry
+	      */)
 {
-char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
-fwrite(message, sizeof(char), sizeof(message), stderr);
+char *message = "and that piece of art is useful\"- Dora Korpar, 2015-10-19\n";
+write(2, message, 58);
 return (1);
 }
