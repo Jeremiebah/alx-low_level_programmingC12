522271a7208b94c9132441dcce159b449a58b725
jeremie <jeremiahbht@gmail.com>
2023-02-20 10:14:09 +0200

    helloworld

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,7 +10,5 @@ printf("Size of int: %zu bytes\n", sizeof(int));
 printf("Size of long: %zu bytes\n", sizeof(long));
 printf("Size of long long: %zu bytes\n", sizeof(long long));
 printf("Size of float: %zu bytes\n", sizeof(float));
-printf("Size of double: %zu bytes\n", sizeof(double));
-printf("Size of long double: %zu bytes\n", sizeof(long double));
 return (0);
 }
