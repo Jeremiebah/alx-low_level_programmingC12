d4158de4c9f7c05b0e07926518ebee736225e5de
jeremie <jeremiahbht@gmail.com>
2023-02-27 21:06:24 +0200

    0x05. C - Pointers, arrays and strings

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+/*header*/
+void print_rev(char *s /**
+			*main entry
+			*/)
+{
+int length = 0;
+char *t = s;
+while (*t != '\0')
+{
+length++;
+t++;
+}
+for (int i = length - 1; i >= 0; i--)
+{
+putchar(s[i]);
+}
+putchar('\n');
+}
