e7acfce6a0dd98562ec7e5c7d9d193a223435f52
jeremie <jeremiahbht@gmail.com>
2023-02-27 21:46:38 +0200

    0x05. C - Pointers, arrays and strings

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+/*header*/
+#include <stdio.h>
+
+void puts2(char *str /**
+		      *main entry
+		      */)
+{
+int i = 0;
+
+while (str[i] != '\0')
+{
+if (i % 2 == 0)
+{
+putchar(str[i]);
+}
+i++;
+}
+putchar('\n');
+}
