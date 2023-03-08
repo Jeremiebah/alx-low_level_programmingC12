ddd5bb32c8415d412923936e30f5d2284a6fa411
jeremie <jeremiahbht@gmail.com>
2023-03-08 21:23:11 +0200

    recursions

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,32 @@
+/* */
+#include <stdio.h>
+
+int _sqrt_recursion(int n /**
+			   *
+			   */)
+{
+if (n < 0)
+{
+return (-1);
+}
+else if (n == 0 || n == 1)
+{
+return (n);
+}
+else
+{
+int i;
+for (i = 1; i * i <= n; i++)
+	{
+	}
+i--;
+if (i *i == n)
+{
+return (i);
+}
+else
+{
+return (-1);
+}
+}
+}
