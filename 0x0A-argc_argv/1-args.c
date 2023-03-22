3110b5f8885d80e1d236e887a187c7f9f210cca5
jeremie <jeremiahbht@gmail.com>
2023-03-22 13:40:29 +0200

    argc and argv

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,6 +1,12 @@
+/* */
 #include <stdio.h>
 
-int main(int argc, char **argv) {
-    printf("%d\n", argc - 1);
-    return 0;
+int main(int argc __attribute__((unused/**
+					*
+				     */)), char **argv __attribute__((unused/**
+			      *
+			      */)))
+{
+printf("%d\n", argc - 1);
+return (0);
 }
