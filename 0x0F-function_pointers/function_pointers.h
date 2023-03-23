d0a73251be344d70c5cc9d392ec4a21ae842cb54
jeremie <jeremiahbht@gmail.com>
2023-03-23 08:14:15 +0200

    functions

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,13 +1,7 @@
-#include <unistd.h>
+#ifndef FUNCTION_POINTERS_H
+#define FUNCTION_POINTERS_H
 
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
+void print_name(char *name, void (*f)(char *));
+int _putchar(char c);
+
+#endif
